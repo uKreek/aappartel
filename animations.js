44cc72e1b8ad5ec32cbd1f647710f86ec5a36e5e
Jasternaut <patrushev2006@list.ru>
2025-02-27 23:35:59 +0200

    [Add] Добавлены свежие изменения.

--- animations.js
+++ animations.js
@@ -1,3 +1,11 @@
+aparts = {
+	"apart0": ["./apartments/apartment/apart0.jpg", "./apartments/apartment/apart1.jpg"],
+	"apart1": [""],
+	"apart2": ["./apartments/apartment-family/apart0.jpg", "./apartments/apartment-family/apart0.jpg"]
+}
+images = aparts[0];
+
+// анимация для линий
 (function () {
 	var rects = document.querySelectorAll('.tittle-line');
 
@@ -17,9 +25,9 @@
 
 })();
 
-
+// анимация для текста артиклей
 (function () {
-	var rects = document.querySelectorAll('.tittle');
+	var rects = document.querySelectorAll('.tittle, #contact-map');
 
 	for (let i = 0; i < rects.length; i++){
 
@@ -37,47 +45,17 @@
 
 })();
 
-
-(function () {
-	var rects = document.querySelectorAll('#contact-map');
-
-	for (let i = 0; i < rects.length; i++){
-
-		var observer = new IntersectionObserver(entries => {
-			entries.forEach(entry => {
-
-				if (entry.isIntersecting) {
-					entry.target.style.scale = '1 1';
-				}
-			});
-		});
-
-		observer.observe(rects[i]);
-	}
-
-})();
-
-
-
-
-
-
-
+// открытие просмотра картинок выбранного апартамента
 function ShowPopup(apartId){
-
-	aparts = {
-		"apart0": ['1.png', '2.png'],
-		"apart1": ['4.png']
-	}
-
 	document.getElementById('popup-room-wrapper').style.display = '';
-	images = aparts[apartId]
-	console.log(apartId)
-
+	images = aparts[apartId];
 }
 
+// закрытие просмотра картинок
 const parentDiv = document.getElementById('popup-room-wrapper');
 const childDiv = document.getElementById('popup-room');
+const previous = document.getElementById('previous');
+const next = document.getElementById('next');
 
 parentDiv.addEventListener('click', () => {
   console.log('Функция родительского div вызвана');
@@ -89,11 +67,37 @@ childDiv.addEventListener('click', (event) => {
   console.log('Функция дочернего div вызвана');
 });
 
+previous.addEventListener('click', (event) => {
+	event.stopPropagation(); // Останавливаем всплытие события
+	Slyde('back');
+	console.log('Функция prev вызвана');
+});
+
+next.addEventListener('click', (event) => {
+	event.stopPropagation(); // Останавливаем всплытие события
+	Slyde("next");
+	console.log('Функция next вызвана');
+});
+
 function HidePopup(){
 	document.getElementById('popup-room-wrapper').style.display = 'none';
 
 }
 
-
-
-
+// перелистывание картинок
+now = 0
+///current = images[now]
+function Slyde(type){
+	var img = document.getElementById('popup-room-img');
+	switch (type){
+		case 'next':
+			if (now+1 <= images.length)
+				current = images[now+1]
+		break;
+		case 'back':
+			if (now-1 >= 0)
+				current = images[now-1]
+		break;
+	}
+	img.style.src = current;
+}
