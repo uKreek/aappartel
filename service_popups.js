8cecd4d3d3ea5c9cab0f7573aefba292d62c584e
Jasternaut <patrushev2006@list.ru>
2025-03-04 20:41:54 +0200

    [Add] Добавлены свежие изменения

--- service_popups.js
+++ service_popups.js
@@ -1,6 +1,57 @@
+
+
 function ShowCarousel(name)
 {
   // .service-popup-center
-  var popup = document.getElementById(name);
+  let popup = document.getElementById(name);
   popup.classList.add("service-popup-center");
 }
+
+
+
+
+
+class Carousel {
+  constructor () {
+    this.popups = document.getElementsByClassName('service-popup')
+    this.popups_len = this.popups.length
+    this.center = null
+    this.left = null
+    this.right = null
+    this.hidden_left = null
+    this.hidden_right = null
+    this.nowId
+    console.log(this.popups)
+  }
+
+  next = () => {
+    nowId += 1;
+  }
+
+
+  prev = () => {
+  
+  }
+
+
+  show = (id) => {
+    console.log("Я НЕ СРАБОТАЮ");
+    let popup = document.getElementById(id);
+    if (this.center != null && this.center.classList.contains("service-popup-center")) {
+      this.center.classList.remove("service-popup-center")
+    }
+    popup.classList.add("service-popup-center");
+    this.center = popup
+
+    if (this.right != null && this.right.classList.contains("service-popup-right")) {
+      this.right.classList.remove("service-popup-right")
+    }
+    this.popups[center_index+1]
+
+
+  }
+
+}
+
+
+const carousel = new Carousel()
