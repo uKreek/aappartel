6d37076d9c295f83f0014fd644d2643ebda5a044
Jasternaut <patrushev2006@list.ru>
2025-03-27 21:00:08 +0200

    [Add] Переделан скроллинг картинок
    Теперь имеется анимация смены картинок, и кнопки теперь функционируют. Если показываемая картинка соответствует кнопке, которая её вызывает, то эта кнопка изменяется в стиле с целью её выделения.

--- js_scripts/image_slider.js
+++ js_scripts/image_slider.js
@@ -1,25 +1,60 @@
+class Slider {
+    constructor() {
+        this.currentIndex = 0; // начинаем с первого слайда
+        this.slides = document.querySelectorAll('.top-img-slide');
+        this.buttons = document.querySelectorAll('.slider-button');
+
+        this.init();
+    }
+
+    init() {
+        this.addEventListeners();
+        this.startAutoSlide();
+        this.updateActiveButton();
+    }
+
+    // добавление листенеров для кнопок
+    addEventListeners() {
+        this.buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                const index = parseInt(button.getAttribute('data-index'));
+                this.changeSlide(index);
+            });
+        });
+    }
+
+    // зменение текущего изображения по индексу
+    changeSlide(index) {
+        this.currentIndex = index; // переход к следующему слайду
+        const offset = -this.currentIndex * 100; // смещение для слайдов
+        for (let i = 0; i < this.slides.length; i++) {
+            this.slides[i].style.transform = `translateX(${offset}%)`;
+        }
+        this.updateActiveButton();
+    }
+
+    // выделяет кнопку, соответствующую картинке по индексу
+    updateActiveButton() {
+        this.buttons.forEach((button, i) => {
+            if (i === this.currentIndex) {
+                button.classList.add('active');
+            } else {
+                button.classList.remove('active');
+            }
+        });
+    }
+
+    // автоматическая прокрутка картинок
+    startAutoSlide() {
+        setInterval(() => {
+            this.currentIndex = (this.currentIndex + 1) % this.slides.length; // Переход к следующему слайду
+            this.changeSlide(this.currentIndex);
+        }, 5000);
+    }
+}
+
+const image_slider = new Slider();
+
 document.addEventListener('DOMContentLoaded', function() {
-    let currentIndex = 0;
-    const slide = document.querySelector('#top_img'); // Используем querySelector для одного элемента
-
-    const images = [
-        "./apartments/apartment/0.jpg",
-        "./apartments/apartment-family/apart0.jpg",
-        "./apartments/apartment-raum/0.jpg"
-    ];
-
-    function showSlide(index) {
-        slide.style.backgroundImage = `url(${images[index]})`;
-    }
-
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % images.length; // Изменено на images.length
-        showSlide(currentIndex);
-    }
-
-    // Инициализация слайдов
-    showSlide(currentIndex);
-    // Переключение изображений каждые 3 секунды
-    setInterval(nextSlide, 3000);
+    image_slider;
 });
-
